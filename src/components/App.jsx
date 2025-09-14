cdc2da505e1c9d4f3c13ce90881e1db6ca80b703
federico <fgiannini95@gmail.com>
2025-09-14 21:02:16 +0100

    Set up the navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,20 @@
-export default function App () {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./Home";
+import { Players } from "./Players";
+import { Navbar } from "./Navbar";
+import { BasketTeams } from "./BasketTeams";
+
+export default function App() {
   return (
-    <div>
-      Hash History Basketball League
-    </div>
-  )
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/players" element={<Players />} />
+          <Route path="/teams" element={<BasketTeams />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
---
+++ src/components/BasketTeams.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const BasketTeams = () => {
+  return <div className="container">Teams</div>;
+};
