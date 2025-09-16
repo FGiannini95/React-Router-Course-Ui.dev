64a18166a1c1e89261392b56712af59bdda56cae
federico <fgiannini95@gmail.com>
2025-09-16 17:06:27 +0100

    Handle all the conditions

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import useArticle from "../hooks/useArticle";
 import { Loading } from "./Loading";
 
@@ -7,16 +7,19 @@ export const Article = () => {
   const { teamId, articleId } = useParams();
   const { response: article, loading } = useArticle({ teamId, articleId });
 
-  return (
-    <div className="panel">
-      {loading ? (
-        <Loading />
-      ) : (
-        <article className="article">
-          <h1 className="header">{article.title}</h1>
-          <p>{article.body}</p>
-        </article>
-      )}
-    </div>
-  );
+  let body;
+  if (loading === true) {
+    body = <Loading />;
+  } else if (article === null) {
+    body = <Navigate to={`/${teamId}/articles`} />;
+  } else {
+    body = (
+      <article className="article">
+        <h1 className="header">{article.title}</h1>
+        <p>{article.body}</p>
+      </article>
+    );
+  }
+
+  return <div className="panel">{body}</div>;
 };
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, Navigate } from "react-router-dom";
 import usePlayer from "../hooks/usePlayer";
 import { Loading } from "./Loading";
 
@@ -8,50 +8,54 @@ export const Player = () => {
 
   const { response: player, loading } = usePlayer(playerId);
 
-  if (loading) {
-    return <Loading />;
+  let body;
+  if (loading === true) {
+    body = <Loading />;
+  } else if (player === null) {
+    body = <Navigate to="/players" />;
+  } else {
+    body = (
+      <>
+        <img
+          src={player.avatar}
+          alt={`Avatar for ${player.name}`}
+          className="avatar"
+        />
+        <h1 className="medium-header">{player.name}</h1>
+        <h3 className="header">#{player.number}</h3>
+        <div className="row">
+          <ul className="info-list" style={{ marginRight: 80 }}>
+            <li>
+              Team
+              <div>
+                {/* Team's name but with first letter capitalized */}
+                <Link to={`/${player.teamId}`}>
+                  {player.teamId[0].toUpperCase() + player.teamId.slice(1)}
+                </Link>
+              </div>
+            </li>
+            <li>
+              Position<div>{player.position}</div>
+            </li>
+            <li>
+              Points PG<div>{player.ppg}</div>
+            </li>
+          </ul>
+          <ul className="info-list">
+            <li>
+              Assists PG<div>{player.apg}</div>
+            </li>
+            <li>
+              Steals RG<div>{player.srg}</div>
+            </li>
+            <li>
+              Rebounce PG<div>{player.rpg}</div>
+            </li>
+          </ul>
+        </div>
+      </>
+    );
   }
-  if (!player) return;
 
-  return (
-    <div className="panel">
-      <img
-        src={player.avatar}
-        alt={`Avatar for ${player.name}`}
-        className="avatar"
-      />
-      <h1 className="medium-header">{player.name}</h1>
-      <h3 className="header">#{player.number}</h3>
-      <div className="row">
-        <ul className="info-list" style={{ marginRight: 80 }}>
-          <li>
-            Team
-            <div>
-              {/* Team's name but with first letter capitalized */}
-              <Link to={`/${player.teamId}`}>
-                {player.teamId[0].toUpperCase() + player.teamId.slice(1)}
-              </Link>
-            </div>
-          </li>
-          <li>
-            Position<div>{player.position}</div>
-          </li>
-          <li>
-            Points PG<div>{player.ppg}</div>
-          </li>
-        </ul>
-        <ul className="info-list">
-          <li>
-            Assists PG<div>{player.apg}</div>
-          </li>
-          <li>
-            Steals RG<div>{player.srg}</div>
-          </li>
-          <li>
-            Rebounce PG<div>{player.rpg}</div>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+  return <div className="panel">{body}</div>;
 };
